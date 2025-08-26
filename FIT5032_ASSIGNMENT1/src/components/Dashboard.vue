848612986d2f00da395326b8d0cc0140d6768f95
srath0029 <samvrathore@gmail.com>
2025-08-26 14:53:55 +1000

    Added Template to Dashboard

---
+++ FIT5032_ASSIGNMENT1/src/components/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card p-4 shadow-sm">
+    <h3>Registered Users</h3>
+    <table class="table table-striped table-hover mt-3" v-if="users.length > 0">
+      <thead class="table-dark">
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Age</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in users" :key="index">
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.age }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>No users registered yet.</p>
+  </div>
+</template>
