a3b38ab045a421a49dd50fdb261b4a074c7ce18c
srath0029 <samvrathore@gmail.com>
2025-08-26 14:55:17 +1000

    Added all Neccessary Imports to Main

--- FIT5032_ASSIGNMENT1/src/main.js
+++ FIT5032_ASSIGNMENT1/src/main.js
@@ -1,12 +1,6 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount("#app");
