0ec079a2a7162580b367d75f13a015f73fcdd0dc
srath0029 <samvrathore@gmail.com>
2025-08-27 15:18:34 +1000

    fixed errors on last Dashboard

--- FIT5032_ASSIGNMENT1/src/components/Dashboard.vue
+++ FIT5032_ASSIGNMENT1/src/components/Dashboard.vue
@@ -11,51 +11,58 @@
       />
     </div>
 
-    <div class="table-responsive">
-      <table class="table table-striped table-hover mt-1" v-if="filtered.length > 0">
-        <thead class="table-dark">
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Username</th>
-            <th scope="col">Email</th>
-            <th scope="col">Age</th>
-            <th scope="col">Location</th>
-            <th scope="col">Gender</th>
-            <th scope="col" style="min-width: 240px;">Reason</th>
-            <th scope="col" style="width: 90px;">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(user, index) in filtered" :key="index">
-            <td>{{ user.firstName }} {{ user.lastName }}</td>
-            <td>{{ user.username }}</td>
-            <td>{{ user.email }}</td>
-            <td>{{ user.age }}</td>
-            <td>{{ user.location }}</td>
-            <td>{{ user.gender }}</td>
-            <td class="text-truncate" style="max-width: 280px;" :title="user.reason">
-              {{ user.reason }}
-            </td>
-            <td>
-              <button
-                class="btn btn-sm btn-danger"
-                @click="confirmDelete(index)"
-                aria-label="Delete this user"
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
 
-    <p v-else class="mb-0">No users registered yet.</p>
+    <template v-if="filtered.length > 0">
+      <div class="table-responsive">
+        <table class="table table-striped table-hover mt-1">
+          <thead class="table-dark">
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Age</th>
+              <th scope="col">Location</th>
+              <th scope="col">Gender</th>
+              <th scope="col" style="min-width: 240px;">Reason</th>
+              <th scope="col" style="width: 90px;">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(user, index) in filtered" :key="index">
+              <td>{{ user.firstName }} {{ user.lastName }}</td>
+              <td>{{ user.username }}</td>
+              <td>{{ user.email }}</td>
+              <td>{{ user.age }}</td>
+              <td>{{ user.location }}</td>
+              <td>{{ user.gender }}</td>
+              <td class="text-truncate" style="max-width: 280px;" :title="user.reason">
+                {{ user.reason }}
+              </td>
+              <td>
+                <button
+                  class="btn btn-sm btn-danger"
+                  @click="confirmDelete(index)"
+                  aria-label="Delete this user"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </template>
+    <template v-else>
+      <p class="mb-0">No users registered yet.</p>
+    </template>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, defineEmits, defineProps, defineOptions } from "vue";
+
+//  Give the component a multi-word name (fixes eslint rule)
+defineOptions({ name: "UsersDashboard" });
 
 const props = defineProps({
   users: { type: Array, required: true }
