5536a13286b53d4fceceb344ebf5ac7a4e5cf466
srath0029 <samvrathore@gmail.com>
2025-08-27 14:20:47 +1000

    added delete to app

--- FIT5032_ASSIGNMENT1/src/App.vue
+++ FIT5032_ASSIGNMENT1/src/App.vue
@@ -3,7 +3,7 @@
     <NavBar />
     <div class="container mt-4">
       <Form @formSubmitted="addUser" />
-      <Dashboard :users="users" />
+      <Dashboard :users="users" @deleteUser="removeUser" />
     </div>
   </div>
 </template>
@@ -14,22 +14,18 @@ import NavBar from "./components/NavBar.vue";
 import Form from "./components/Form.vue";
 import Dashboard from "./components/Dashboard.vue";
 
-// State to hold dynamic data
 const users = ref([]);
 
-// Add user from form submission
 const addUser = (user) => {
-  users.value.push({
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    age: user.age,
-  });
-  // Save to LocalStorage (persistence beyond session)
+  users.value.push(user);
   localStorage.setItem("users", JSON.stringify(users.value));
 };
 
-// Load existing users if any
+const removeUser = (index) => {
+  users.value.splice(index, 1);
+  localStorage.setItem("users", JSON.stringify(users.value));
+};
+
 const storedUsers = localStorage.getItem("users");
 if (storedUsers) {
   users.value = JSON.parse(storedUsers);
