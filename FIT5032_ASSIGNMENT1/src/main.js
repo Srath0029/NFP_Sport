42bea7f96fd99b1033928fa27f3078d2a326fc5a
srath0029 <samvrathore@gmail.com>
2025-09-04 13:59:36 +1000

    updated main to sync with auth

--- FIT5032_ASSIGNMENT1/src/main.js
+++ FIT5032_ASSIGNMENT1/src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { initAuth } from "./composables/auth";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-createApp(App).use(router).mount("#app");
+(async () => {
+  await initAuth();            // seed admin, restore session
+  createApp(App).use(router).mount("#app");
+})();
