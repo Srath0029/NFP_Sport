c68318f9334114d6fd8b4d5a75b669a6a12553ba
srath0029 <samvrathore@gmail.com>
2025-09-07 12:36:52 +1000

    updated index with profile

--- FIT5032_ASSIGNMENT1/src/router/index.js
+++ FIT5032_ASSIGNMENT1/src/router/index.js
@@ -5,6 +5,7 @@ import RegisterView from "../views/RegisterView.vue";
 import AboutView from "../views/AboutView.vue";
 import ContactView from "../views/ContactView.vue";
 import AdminView from "../views/AdminView.vue";
+import ProfileView from "../views/ProfileView.vue";
 import { useAuth } from "../composables/auth";
 
 const routes = [
@@ -13,18 +14,19 @@ const routes = [
   { path: "/register", name: "Register", component: RegisterView },
   { path: "/about", name: "About", component: AboutView },
   { path: "/contact", name: "Contact", component: ContactView },
+  { path: "/profile", name: "Profile", component: ProfileView, meta: { requiresAuth: true } },
   { path: "/admin", name: "Admin", component: AdminView, meta: { requiresAuth: true, roles: ["admin"] } },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior: () => ({ top: 0 })
+  scrollBehavior: () => ({ top: 0 }),
 });
 
 router.beforeEach((to, from, next) => {
   const { currentUser } = useAuth();
-  const user = currentUser.value || JSON.parse(localStorage.getItem("auth_session_v1") || "null");
+  const user = currentUser.value;
 
   if (to.meta?.requiresAuth && !user) {
     return next({ name: "Login", query: { redirect: to.fullPath } });
