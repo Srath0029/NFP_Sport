5d9785e288fe7356b21620c1eef557417506f6a4
srath0029 <samvrathore@gmail.com>
2025-10-08 13:30:41 +1100

    updated main

--- src/main.js
+++ src/main.js
@@ -10,6 +10,11 @@ import { initAuth } from "./composables/auth";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "datatables.net-bs5/css/dataTables.bootstrap5.min.css";
+
+import $ from "jquery";
+import dt from "datatables.net-bs5";
+dt(window, $);
 
 (async () => {
   await initAuth();
