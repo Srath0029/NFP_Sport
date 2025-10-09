134c812a0c57ba8ae0205b96aeeb071d75786cb2
srath0029 <samvrathore@gmail.com>
2025-10-09 13:56:28 +1100

    fixed html

--- src/views/AdminEmailView.vue
+++ src/views/AdminEmailView.vue
@@ -38,7 +38,7 @@
 <script setup>
 import { ref } from "vue";
 import { sendEmailViaHttp, textToHtml } from "../services/emailService";
-import { sendEmailViaHttp } from "@/services/emailService";
+import { sendEmailViaHttp, textToHtml } from "@/services/emailService";
 
 
 const to = ref("");
