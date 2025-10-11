82a945b182a4d97de0374cb3dc2630f7e6521d24
srath0029 <samvrathore@gmail.com>
2025-10-11 15:31:53 +1100

    functions updated

--- .wrangler/tmp/bundle-eIVcJP/middleware-insertion-facade.js
+++ .wrangler/tmp/bundle-eIVcJP/middleware-insertion-facade.js
@@ -1,8 +1,8 @@
-				import worker, * as OTHER_EXPORTS from "C:\\Users\\resol\\FIT5032_Project\\FIT5032_Assignment1\\.wrangler\\tmp\\pages-ZA8KKJ\\functionsWorker-0.9704554600909161.mjs";
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\resol\\FIT5032_Project\\FIT5032_Assignment1\\.wrangler\\tmp\\pages-DPF3Rn\\functionsWorker-0.224568639669102.mjs";
 				import * as __MIDDLEWARE_0__ from "C:\\Users\\resol\\FIT5032_Project\\FIT5032_Assignment1\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
 import * as __MIDDLEWARE_1__ from "C:\\Users\\resol\\FIT5032_Project\\FIT5032_Assignment1\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
 
-				export * from "C:\\Users\\resol\\FIT5032_Project\\FIT5032_Assignment1\\.wrangler\\tmp\\pages-ZA8KKJ\\functionsWorker-0.9704554600909161.mjs";
+				export * from "C:\\Users\\resol\\FIT5032_Project\\FIT5032_Assignment1\\.wrangler\\tmp\\pages-DPF3Rn\\functionsWorker-0.224568639669102.mjs";
 				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
 				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
 					
