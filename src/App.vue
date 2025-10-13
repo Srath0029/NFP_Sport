0ce4a6656e4a3d722f845fa59d87423c8dbb8fa0
srath0029 <samvrathore@gmail.com>
2025-10-13 12:38:25 +1100

    fixed accessability issues

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,12 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
 </script>
+<template>
+  <a href="#main" class="visually-hidden-focusable position-absolute top-0 start-0 m-2 btn btn-warning btn-sm">
+    Skip to content
+  </a>
+  <div id="app">
+    <NavBar />
+    <RouterView />
+  </div>
+</template>
